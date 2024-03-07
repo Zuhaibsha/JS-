554ce34dc6bc5faeed25a44cafb5aa8333cf91d9
Zuhaibsha <zuhaibshah709@gmail.com>
2024-03-07 21:35:47 +0500

    java 3

---
+++ app.js
@@ -0,0 +1,196 @@
+
+
+
+//     chapter 9 to 11
+// User input and conditional statement 
+
+    /// Question 1
+// var city = prompt("Please enter the name of your city:");
+
+ 
+//   if (city === "karachi") {
+  
+//     alert("Welcome to the city of lights!");
+//   }else{
+//     alert("welcome !");
+//   }
+
+  // Question 2
+
+//   var gender=prompt("Enter your gender:");
+
+//   if (gender === "male") {
+//     alert("Good Morning Sir.");
+//   } else if (gender === "female") {
+//     alert("Good Morning Maam.");
+//   } else {
+    
+//     alert("Invalid input. Please enter either 'male' or 'female'.");
+//   }
+
+//Question 3
+ 
+// var color=prompt("Enter color:")
+
+// if(color ==="red"){
+//     alert("Must stop")
+// }
+// else if(color ==="green"){
+//     alert("Move no")
+// }
+// else if(color ==="orange"){
+//     alert("Ready to move ")
+// }
+
+//Question 4
+
+// var fuel=prompt("Enter your remaining fuel in litre :")
+
+// if(fuel < 0.25){
+// alert("Please refill your fuel !!")
+
+// }else{
+//     alert("Ready to go")
+// }
+
+
+//Question 5
+//var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// } if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+
+// Question 8
+
+// var number = prompt("Please enter a number:");
+
+
+// if (number % 3 === 0) {
+//   alert(number + " is divisible by 3.");
+// } else {
+//   alert(number + " is not divisible by 3.");
+// }
+
+//Question 9 
+// var number=prompt("please enter number:");
+
+// if(number % 2===0){
+// alert(number  +  " is even");
+// }else{
+//     alert("your number is odd");
+// }
+
+//Question 10
+
+// var temperature=prompt("Enter temperature:");
+
+// if(temperature > 40){
+// alert("Its hot out side")
+// }
+// else if(temperature > 30){
+//     alert("the weather is normal today")
+//     }
+//     else if(temperature > 20){
+//         alert("Today's weather is little bit  cold")
+//         }
+//         else if(temperature < 10){
+//             alert("OMG its cold today");
+//             }
+//             else if(temperature > 10){
+//                 alert("OMG its cold today");
+//                 }
+//             else{
+//                 alert("required temperature ")
+//             }
+
+// var num=+prompt("Enter value");
+// var num2=+prompt("Enter 2nd value");
+// var operator=prompt();
+
+
+//Question 10 
+
+// // if Statements 
+// if(operator=="+"){
+//     document.write(num +num2);
+// }
+// else if(operator=="-"){
+
+//     document.write(num -num2);
+// }
+// else if(operator=="*"){
+
+//     document.write(num * num2);
+// }
+// else if(operator=="/"){
+
+//     document.write(num /num2);
+// }
+// else if(operator=="%"){
+
+//     document.write(num %num2);
+// }
+// else{
+
+//     document.write("enter a valid operator");
+// }
+
+
+// chapter 11-13
+//Question 6
+// var hour=prompt("enter hour:");
+
+// if (hour < 18) {
+// document.write("Good morning");
+// }
+// else{
+// document.write("Good evening")
+// }
+
+//Question 3
+
+// var number=prompt("Enter number");
+// if(number>0){
+// alert("number is positive")
+
+// }
+// else if(number>0){
+//   alert("number is negative")
+  
+// }
+// else if(number==0){
+//   alert("number is zero")
+  
+// }
+// else{
+//   alert("Enter a number");
+// }
+
+// Question 5
+  // var correctPassword="password123";
+  // var userPassword = prompt("Please enter your password:");
+
+  
+  // if (!userPassword) {
+  //   alert("Please enter your password.");
+  // } else if (userPassword === correctPassword) {
+   
+  //   alert("Correct! you enter a correct password.");
+  // } else {
+    
+  //   alert("Incorrect password.");
+  // }
+
+  // Question 7
+
+  
+  
+ 
